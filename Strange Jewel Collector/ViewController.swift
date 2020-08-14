9a2b24f572aa8831b81a75fa27506828c6cbcab0
SwiftCoder <SwiftCoder@nossvwpdf.ls>
2020-08-14 18:39:37 +0300

    Initial Commit

---
+++ Strange Jewel Collector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Strange Jewel Collector
+//
+//  Created by Viktor on 14.08.2020.
+//  Copyright © 2020 Viktor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
